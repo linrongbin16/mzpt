d7bea82e3d64b597e23ff709aac6380a0dd6df98
linrongbin16 <linrongbin16@outlook.com>
2022-04-29 18:02:39 +0800

    optimize zsh start up

--- zsh-prompt.sh
+++ zsh-prompt.sh
@@ -1,85 +1,19 @@
-# zsh theme
-_detect_conda() {
-    if [[ -n "$CONDA_PREFIX" ]]; then
-        local conda_name="$(basename $CONDA_PREFIX)"
-        if [[ "$conda_name" == "miniconda3" ]]; then
-            # Without this, it would display conda version
-            _PROMPT_CONDA_ENV="( base) "
-        else
-            # For all environments that aren't (base)
-            _PROMPT_CONDA_ENV="( $conda_name) "
-        fi
-    else
-        # When no conda environment is active, don't show anything
-        _PROMPT_CONDA_ENV=""
-    fi
+
+# autoload function path
+fpath=($fpath ~/.zsh-prompt)
+autoload -Uz _detect_conda
+autoload -Uz _detect_git
+autoload -Uz _detect_os
+
+_detect() {
+    _detect_conda
+    _detect_git
+    _detect_os
 }
 
-_detect_git() {
-    local git_branch="$(git branch --show-current 2>/dev/null)"
-    if [ ! -z "$git_branch" ]; then
-        if [ ! -z "$(git ls-files --exclude-standard --others -md)" ]; then
-            _PROMPT_GIT_ENV="( $git_branch*)"
-        else
-            _PROMPT_GIT_ENV="( $git_branch)"
-        fi
-    else
-        _PROMPT_GIT_ENV=""
-    fi
-}
-
-_detect_os() {
-    # cache OS env since it's not change
-    if [ ! -z "$_PROMPT_OS_ENV" ]; then
-        return
-    fi
-    local os_name="$(uname -s)"
-    case "$os_name" in
-        Linux)
-            if [ -f "/etc/arch-release" ] || [ -f "/etc/artix-release" ]; then
-                _PROMPT_OS_ENV=" "
-            elif [ -f "/etc/fedora-release" ]; then
-                _PROMPT_OS_ENV=" "
-            elif [ -f "/etc/redhat-release" ]; then
-                _PROMPT_OS_ENV=" "
-            elif [ -f "/etc/gentoo-release" ]; then
-                _PROMPT_OS_ENV=" "
-            elif [ -f "/etc/os-release" ]; then
-                if cat /etc/os-release | grep Ubuntu >/dev/null 2>&1; then
-                    _PROMPT_OS_ENV=" "
-                elif cat /etc/os-release | grep Debian >/dev/null 2>&1; then 
-                    _PROMPT_OS_ENV=" "
-                elif cat /etc/os-release | grep CentOS >/dev/null 2>&1; then 
-                    _PROMPT_OS_ENV=" "
-                else
-                    _PROMPT_OS_ENV=" "
-                fi
-            else
-                _PROMPT_OS_ENV=" "
-            fi
-            ;;
-        *BSD)
-            _PROMPT_OS_ENV=" "
-            ;;
-        Darwin)
-            _PROMPT_OS_ENV=" "
-            ;;
-        WindowsNT)
-            _PROMPT_OS_ENV=" "
-            ;;
-        *)
-            _PROMPT_OS_ENV=" "
-            ;;
-    esac
-}
-
-# run before each command
-precmd_functions+=( _detect_conda )
-precmd_functions+=( _detect_git )
-precmd_functions+=( _detect_os )
+precmd_functions+=( _detect )
 
 setopt prompt_subst
 
-export RPROMPT='%F{#FF8000}${_PROMPT_CONDA_ENV}%f%D{%H:%M}'
-export PROMPT='%F{cyan}%n%f@%F{#9933FF}%M%f %F{blue}${_PROMPT_OS_ENV}%~%f %F{magenta}${_PROMPT_GIT_ENV}%f [%(?.%F{green}√.%F{red}?%?)%f]
+export PROMPT='%F{#FF8000}${_PROMPT_CONDA_ENV}%f%F{cyan}%n%f@%F{#9933FF}%M%f %F{blue}${_PROMPT_OS_ENV}%~%f %F{magenta}${_PROMPT_GIT_ENV}%f%D{%H:%M} [%(?.%F{green}√.%F{red}?%?)%f]
 %F{blue}%(!.#.$)%f '
