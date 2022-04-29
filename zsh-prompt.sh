9c299f8d8a8e709936d83f5b6ad60866c07f0b72
linrongbin16 <linrongbin16@outlook.com>
2022-04-29 10:50:03 +0800

    optimize performance

--- zsh-prompt.sh
+++ zsh-prompt.sh
@@ -1,14 +1,14 @@
 # zsh theme
 my_precmd_detect_conda() {
-    if [[ -n $CONDA_PREFIX ]]; then
-        if [[ $(basename $CONDA_PREFIX) == "miniconda3" ]]; then
+    if [[ -n "$CONDA_PREFIX" ]]; then
+        local conda_name="$(basename $CONDA_PREFIX)"
+        if [[ "$conda_name" == "miniconda3" ]]; then
             # Without this, it would display conda version
-            MY_PROMPT_CONDA_ENV="base"
+            MY_PROMPT_CONDA_ENV="( base)"
         else
             # For all environments that aren't (base)
-            MY_PROMPT_CONDA_ENV="$(basename $CONDA_PREFIX)"
+            MY_PROMPT_CONDA_ENV="( $conda_name)"
         fi
-        MY_PROMPT_CONDA_ENV="( ${MY_PROMPT_CONDA_ENV}) "
     else
         # When no conda environment is active, don't show anything
         MY_PROMPT_CONDA_ENV=""
@@ -16,28 +16,28 @@ my_precmd_detect_conda() {
 }
 
 my_precmd_detect_git() {
-    if git status >/dev/null 2>&1; then
-        MY_PROMPT_GIT_BRANCH_ENV="$(git symbolic-ref HEAD 2>/dev/null|cut -d/ -f3-)"
-        if [ ! -z "$(git ls-files -d)" ]; then
-            MY_PROMPT_GIT_BRANCH_ENV="${MY_PROMPT_GIT_BRANCH_ENV}*"
-        elif [ ! -z "$(git ls-files -m)" ]; then
-            MY_PROMPT_GIT_BRANCH_ENV="${MY_PROMPT_GIT_BRANCH_ENV}*"
-        elif [ ! -z "$(git ls-files . --exclude-standard --others)" ]; then
-            MY_PROMPT_GIT_BRANCH_ENV="${MY_PROMPT_GIT_BRANCH_ENV}*"
+    local git_branch_name="$(git branch --show-current 2>/dev/null)"
+    if [ ! -z "$git_branch_name" ]; then
+        if [ ! -z "$(git ls-files --exclude-standard --others -md)" ]; then
+            MY_PROMPT_GIT_BRANCH_ENV="( $git_branch_name*)"
+        else
+            MY_PROMPT_GIT_BRANCH_ENV="( $git_branch_name)"
         fi
-        MY_PROMPT_GIT_BRANCH_ENV="( ${MY_PROMPT_GIT_BRANCH_ENV}) "
     else
         MY_PROMPT_GIT_BRANCH_ENV=""
     fi
 }
 
 my_precmd_detect_os() {
-    local MY_PROMPT_OS_NAME="$(uname -s)"
-    MY_PROMPT_OS_ENV=""
-    case "$MY_PROMPT_OS_NAME" in
+    # cache OS env since it's not change
+    if [ ! -z "$MY_PROMPT_OS_ENV" ]; then
+        return
+    fi
+    local os_name="$(uname -s)"
+    case "$os_name" in
         Linux)
             if [ -f "/etc/arch-release" ] || [ -f "/etc/artix-release" ]; then
-                    MY_PROMPT_OS_ENV=" "
+                MY_PROMPT_OS_ENV=" "
             elif [ -f "/etc/fedora-release" ]; then
                 MY_PROMPT_OS_ENV=" "
             elif [ -f "/etc/redhat-release" ]; then
