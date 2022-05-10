007b0a248c0fa7f128300e52206408addea67008
linrongbin16 <linrongbin16@outlook.com>
2022-05-10 09:27:43 +0800

    update

--- mzpt.sh
+++ mzpt.sh
@@ -45,8 +45,8 @@ _mzpt_git() {
 }
 
 _mzpt_os() {
-    local _os_name="$(uname -s)"
-    case "$_os_name" in
+    local os_name="$(uname -s)"
+    case "$os_name" in
         Linux)
             if [ -f /etc/arch-release ] || [ -f /etc/artix-release ]; then
                 _MZPT_OS=" "
@@ -89,27 +89,27 @@ _mzpt_os() {
 }
 
 _mzpt_hostname() {
-    local _hostname=$(hostname)
-    _MZPT_HOSTNAME_LEN=${#_hostname}
+    local hostname_value=$(hostname)
+    _MZPT_HOSTNAME_LEN=${#hostname_value}
 }
 
 _mzpt_exit_code() {
-    _MZPT_SAVED_EXIT_CODE=$?
-    _MZPT_SAVED_EXIT_CODE_LEN=${#_MZPT_SAVED_EXIT_CODE}
-    if [ $_MZPT_SAVED_EXIT_CODE -ne 0 ]; then
-        _MZPT_SAVED_EXIT_CODE_LEN=$(($_MZPT_SAVED_EXIT_CODE_LEN+1))
+    _MZPT_EXIT_CODE=$?
+    _MZPT_EXIT_CODE_LEN=${#_MZPT_EXIT_CODE}
+    if [ $_MZPT_EXIT_CODE -ne 0 ]; then
+        _MZPT_EXIT_CODE_LEN=$(($_MZPT_EXIT_CODE_LEN+1))
     fi
 }
 
 _mzpt_delimiter() {
-    local _username_len=${#USERNAME}
-    local _dir_value=$(dirs)
-    local _dir_len=${#_dir_value}
-    local _left_len=$(($_username_len+1+$_MZPT_HOSTNAME_LEN+1+$_MZPT_OS_LEN+$_dir_len+1+$_MZPT_GIT_LEN+2+$_MZPT_SAVED_EXIT_CODE_LEN))
-    local _time_len=5
-    local _right_len=$((1+$_MZPT_CONDA_LEN+1+$_time_len))
-    local _delimiter_len=$(($COLUMNS-$_left_len-$_right_len))
-    _MZPT_DELIMITER=$(printf "%${_delimiter_len}s" | tr " " "─")
+    local username_len=${#USERNAME}
+    local dir_value=$(dirs)
+    local dir_len=${#dir_value}
+    local left_len=$(($username_len+1+$_MZPT_HOSTNAME_LEN+1+$_MZPT_OS_LEN+$dir_len+1+$_MZPT_GIT_LEN+2+$_MZPT_EXIT_CODE_LEN))
+    local time_len=5
+    local right_len=$((1+$_MZPT_CONDA_LEN+1+$time_len))
+    local delimiter_len=$(($COLUMNS-$left_len-$right_len))
+    _MZPT_DELIMITER=$(printf "%${delimiter_len}s" | tr " " "─")
 }
 
 _mzpt_precmd() {
